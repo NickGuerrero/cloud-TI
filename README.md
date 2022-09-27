8c56af8269bf85dd6147c56422d15e877f084802
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2022-09-27 15:24:13 -0700

    Add testing folder and begin some of the unit tests
    Readme has been updated to show the desired structure.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 ## Cloud testing for talent initiative project
 Please contact repository owner for further details.
+
+Project Structure (WIP):
+
+    cloud-ti
+    ├── app
+    |   ├── slack-listener (package)
+    |   └── group-queue (package)
+    ├── test (package)
+    ├── docker-compose.yml
+    └── README.md
+
+Please file any suggestions, comments, or complaints to the issues board
---
+++ test/test-grouping.py
@@ -0,0 +1,18 @@
+# import the functions
+# TODO: Convert application into packages for importing into unit tests
+
+def dummyreq(id_no, meet_type, meet_size, topic):
+    return {"slack_id": id_no, "meeting_type": meet_type, "meeting_size": meet_size, "topic": topic}
+
+# TESTING
+# TODO: Build proper testing classes
+def test_basic():
+    grp_no = [2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4]
+    met_typ = ["mock_interview" if x < 11 else "group_problem_solving" for x in range(16)]
+    x = [dummyreq(id, met_typ[id], grp_no[id], "arrays") for id in range(len(grp_no))]
+    # Insert into group function (Use the better grouping system if we have it ready)
+    # TODO: What do you want the deterministic output to be?
+
+if __name__ == "__main__":
+    pass # Tests go here
+    print("All tests passed")
