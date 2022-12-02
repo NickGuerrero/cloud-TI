992200b32780c6c074e6aa134eb9b16bdaa936d7
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2022-12-02 15:46:15 -0800

    Initialize test package

--- test/test-grouping.py
+++ test/test-grouping.py
@@ -1,4 +1,4 @@
-# import the functions
+import SimpleGrouper
 # TODO: Convert application into packages for importing into unit tests
 
 def dummyreq(id_no, meet_type, meet_size, topic):
@@ -11,6 +11,7 @@ def test_basic():
     met_typ = ["mock_interview" if x < 11 else "group_problem_solving" for x in range(16)]
     x = [dummyreq(id, met_typ[id], grp_no[id], "arrays") for id in range(len(grp_no))]
     # Insert into group function (Use the better grouping system if we have it ready)
+    y = SimpleGrouper.simple_group(x)
     # TODO: What do you want the deterministic output to be?
 
 if __name__ == "__main__":
